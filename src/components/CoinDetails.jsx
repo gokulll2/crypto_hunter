eed963eccc8e5f28b504e90661d47fb6351fc89a
Gokul <gokulsethi69@gmail.com>
2023-05-20 09:24:56 +0530

    main

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,21 +1,23 @@
-import { Box, Container, HStack, Radio, RadioGroup } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Container, HStack, Radio, RadioGroup, VStack , Text , Image } from '@chakra-ui/react'
+// import React from 'react'
 import Loader from './Loader';
 import axios from 'axios';
 import { server } from '..';
 import { useParams } from 'react-router-dom';
 import ErrorComponent from './ErrorComponent';
+import React , { useEffect , useState } from "react"
 function CoinDetails() {
     const [Coin,setCoin]=React.useState([]);
-    const [loading,setLoading]=React.useState(true)
+    const [loading,setLoading]=React.useState(false)
     const[error,setError]=React.useState(false);
     const[currency,setCurrency]=React.useState("inr")
     const params = useParams()
     React.useEffect(() => {
-        const fetchCoin= async() =>{
+        const fetchCoin= async () =>{
             try{
+            setLoading(true)
              const { data } = await axios.get(`${server}/coins/${params.id}`)
- 
+             console.log(data);
              setCoin(data);
              setLoading(false);
             }
@@ -24,19 +26,19 @@ function CoinDetails() {
              setLoading(false);
  
             }
-            fetchCoin()
         }
-    }, [params.id])
+        fetchCoin();
+    }, [])
 
     if(error) return <ErrorComponent message={"Error while fetching coin"}/>
 
   return (
-   <Container maxw={"container.xl"}>
+   <Container maxW={"container.xl"}>
     {
         loading ? <Loader/> : (
             <> 
             <Box borderWidth={1} width={"full"} >
-sddssds
+
             </Box>
 
 
@@ -49,6 +51,18 @@ sddssds
         <Radio value={"eur"}>EUR</Radio>
         </HStack>
         </RadioGroup>
+
+        <VStack spacing={"4"} p={"16"} alignItems={"flex-start"}>
+            <Text fontSize={"small"} alignSelf={"center"} opacity={"0.7"} >
+                Last Updated On {Date().split("G")[0]}
+            </Text>
+
+            <Image 
+            src={Coin.image.large} 
+            w={"16"}
+             h={"16"} 
+            objectFit={"contain"}/>
+        </VStack>
             </>
         )
     }
